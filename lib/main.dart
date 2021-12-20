5067732c72095a58b7f78da21e2cfae5941ee1ca
jjullimayanne <julli.souza@ufob.edu.br>
2021-12-20 01:06:36 -0300

    espçamento

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "frases do dia",
     ///home: Container(color: Colors.white,),
-   home: Container(
-     color: Colors.white,
-     child: Column(
-       children: [
-         Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae nulla vestibulum, consequat tortor a, luctus feliss',
-         style: TextStyle(
-           fontSize: 30,
-           color: Colors.black,
-         ),
-         )
-       ],
-     ),
-
-     ),
+    home: Container(
+      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+      margin: EdgeInsets.all(30),
+      ///margin: EdgeInsets.only()
+      //color: Colors.white,
+      decoration: BoxDecoration(
+        border: Border.all(width: 3, color: Colors.white)
+      ),
+      child: Column(
+        children: [
+          Text("t1"),
+          Padding(padding: EdgeInsets.all(30),
+          child: Text("Texto do padding"),
+           ),
+          Text("t1"),
+          Text("t1"),
+          Text("t1"),
+          /*
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit",
+        textAlign: TextAlign.justify,
+        )
+        */
+        ],
+      ),
+    ),
   ));
 }
