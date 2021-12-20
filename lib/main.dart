df0b134e85141ee04cc605d9f6d7214b92c15f61
jjullimayanne <julli.souza@ufob.edu.br>
2021-12-20 00:15:47 -0300

    start' '

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "frases do dia",
+    ///home: Container(color: Colors.white,),
+    home: Column(children: [
+      Text("usuario:"),
+      Text("senha"),
+    ],),
+  ));
+}
