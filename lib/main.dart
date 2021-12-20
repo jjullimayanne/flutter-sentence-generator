2d85ffa46c223abed9c6532ddcf8d99b2c9830d4
jjullimayanne <julli.souza@ufob.edu.br>
2021-12-20 00:31:58 -0300

    colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(
+  runApp(MaterialApp(
     title: "frases do dia",
     ///home: Container(color: Colors.white,),
-    home: Column(children: [
-      Text("usuario:"),
-      Text("senha"),
-    ],),
+   home: Container(
+     color: Colors.white,
+     child: Column(
+       children: [
+         Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vitae nulla vestibulum, consequat tortor a, luctus feliss',
+         style: TextStyle(
+           fontSize: 30,
+           color: Colors.black,
+         ),
+         )
+       ],
+     ),
+
+     ),
   ));
 }
