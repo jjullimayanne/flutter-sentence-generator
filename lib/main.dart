e666e2e458bf0a8f701fd40c2938b98c0ce81001
jjullimayanne <julli.souza@ufob.edu.br>
2021-12-21 21:10:55 -0300

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "frases do dia",
+    title: "Series de natal",
     ///home: Container(color: Colors.white,),
-    home: Container(
-      margin: EdgeInsets.only(top: 30),
-      decoration: BoxDecoration(
-        border: Border.all(width: 3, color: Colors.white)
-      ),
-      child: Image.asset(
-        "images/foto1.jpg",
-       // fit: BoxFit.contain ,
-        //fit: BoxFit.cover ,
-        // fit: BoxFit.fill,
-         //fit: BoxFit.fitHeight,
-          //fit: BoxFit.fitWidth,
-           //fit: BoxFit.none,
-            fit: BoxFit.scaleDown,
-
-      ),
-    ),
+    home: Scaffold()
+    ,
   ));
 }
